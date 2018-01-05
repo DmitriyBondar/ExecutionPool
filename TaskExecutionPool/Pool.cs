931ec46b03f3f93c064e3013f6edf6037264eddb
DmitriyBondar <bondardmitriya@gmail.com>
2018-01-05 11:10:49 +0200

    Publish

---
+++ TaskExecutionPool/Pool.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace TaskExecutionPool
+{
+    /// <summary>
+    /// Execution pool for Delegates. 
+    /// </summary>
+    public sealed class Pool
+    {
+        private readonly ConcurrentQueue<Action> _delegateQueue;
+
+        private readonly object _startExecutionLock = new object();
+
+        /// <summary>
+        /// Execution pool state
+        /// </summary>
+        public bool Idle { get; private set; }
+
+        /// <summary>
+        /// Count of successfully processed tasks
+        /// </summary>
+        public int ProcessedTasksCount { get; private set; }
+
+        /// <summary>
+        /// New Instance of Task Execution Pool
+        /// </summary>
+        public Pool()
+        {
+            _delegateQueue = new ConcurrentQueue<Action>();
+            Idle = true;
+        }
+
+        /// <summary>
+        /// Executes delegate asynchronously in sequential order
+        /// </summary>
+        public void AddForExecution(Action task)
+        {
+            if (task == null)
+                return;
+
+            _delegateQueue.Enqueue(task);
+
+            lock (_startExecutionLock)
+            {
+                if (Idle)
+                    StartExecution();
+            }
+        }
+
+        /// <summary>
+        /// Starts execution of tasks in queue
+        /// </summary>
+        private async void StartExecution()
+        {
+            Console.WriteLine($"Execution started: {DateTime.Now}");
+
+            Action headTask;
+            if (!_delegateQueue.TryDequeue(out headTask))
+                return;
+
+            Idle = false;
+            await ProcessDelegateAsync(headTask);
+        }
+
+        /// <summary>
+        /// Recursively executes delegates from queue
+        /// </summary>
+        private async Task ProcessDelegateAsync(Action task)
+        {
+            if (task != null)
+            {
+                try
+                {
+                    await Task.Run(task)
+                                .ContinueWith(t => ProcessedTasksCount++, TaskContinuationOptions.OnlyOnRanToCompletion);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+                
+            Action nextTask;
+            if (_delegateQueue.TryDequeue(out nextTask))
+            {
+                await ProcessDelegateAsync(nextTask);
+            }
+            else
+            {
+                Idle = true;
+                Console.WriteLine($"Idle");
+            }
+        }
+
+        /// <summary>
+        /// Count of Execution Queue
+        /// </summary>
+        /// <returns></returns>
+        public int Count()
+        {
+            return _delegateQueue?.Count ?? 0;
+        }
+    }
+}
---
+++ TaskExecutionPoolTests/PoolTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskExecutionPool;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class PoolTests
+    {
+        private Pool _executionPool;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _executionPool = new Pool();
+        }
+
+        [Test]
+        public void AddForExecution_SequentialTaskAdding()
+        {
+            var tasks = GetTasks().ToList();
+            foreach (var task in tasks)
+            {
+                _executionPool.AddForExecution(task);
+            }
+
+            Thread.Sleep(35000);
+
+            Assert.AreEqual(_executionPool.ProcessedTasksCount, tasks.Count);
+        }
+
+        [Test]
+        public void AddForExecution_ConcurrentTaskAdding()
+        {
+            var tasks = GetTasks().ToList();
+
+            Parallel.Invoke(() => _executionPool.AddForExecution(tasks[0]),
+                () => _executionPool.AddForExecution(tasks[1]), () => _executionPool.AddForExecution(tasks[2]),
+                () => _executionPool.AddForExecution(tasks[3]), () => _executionPool.AddForExecution(tasks[4]));
+
+            Thread.Sleep(35000);
+
+            Assert.AreEqual(_executionPool.ProcessedTasksCount, tasks.Count);
+        }
+
+        [Test]
+        public void AddForExecution_TaskWithException_ProcessedTaskCountEquals_3()
+        {
+            var tasks = GetTasksWithException().ToList();
+
+            Parallel.Invoke(() => _executionPool.AddForExecution(tasks[0]),
+                () => _executionPool.AddForExecution(tasks[1]), () => _executionPool.AddForExecution(tasks[2]),
+                () => _executionPool.AddForExecution(tasks[3]));
+
+            Thread.Sleep(11000);
+
+            Assert.AreEqual(_executionPool.ProcessedTasksCount, 3);
+        }
+
+
+
+        private IEnumerable<Action> GetTasks()
+        {
+            return new List<Action>
+            {
+                () =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"First task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(6000);
+                    Console.WriteLine($"Second task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(9000);
+                    Console.WriteLine($"Third task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(12000);
+                    Console.WriteLine($"Fourth task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(4000);
+                    Console.WriteLine($"Fifth task Executed: {DateTime.Now}");
+                }
+            };
+        }
+
+        private IEnumerable<Action> GetTasksWithException()
+        {
+            return new List<Action>
+            {
+                () =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"First task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Second task Executed: {DateTime.Now}");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(3000);
+                    throw new Exception("Very unexpected exception");
+                },
+
+                () =>
+                {
+                    Thread.Sleep(4000);
+                    Console.WriteLine($"Fourth task Executed: {DateTime.Now}");
+                }
+            };
+        }
+    }
+}
